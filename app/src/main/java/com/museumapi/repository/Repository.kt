b7efd89292a9a9feb19e4ab834ddbdbd2c7368a1
Mauro Crispim <mcrispim@hotmail.com>
2023-03-30 22:37:29 -0300

    Cria a função getNObjectsByDepto no repo

--- app/src/main/java/com/museumapi/repository/Repository.kt
+++ app/src/main/java/com/museumapi/repository/Repository.kt
@@ -14,24 +14,30 @@ const val TAG = "Repository"
 
 class Repository () {
     val service = RetrofitInstance.api
-    lateinit var objectIDs: List<Int>
-    init {
+    val objectIDs = getObjectIDs(-1)
+
+    private fun getObjectIDs(deptoID: Int = -1): List<Int>? {
+        var objectIDs: List<Int>? = null
         CoroutineScope(Dispatchers.IO).launch {
             val response = try {
-                service.fetchMuseumObjectIds()
-            } catch(e: IOException) {
+                if (deptoID == -1)
+                    service.fetchMuseumObjectIds()
+                else
+                    service.fetchMuseumObjectIdsByDepto(deptoID)
+            } catch (e: IOException) {
                 Log.e(TAG, "IOException, you might not have internet connection")
                 return@launch
             } catch (e: HttpException) {
                 Log.e(TAG, "HttpException, unexpected response")
                 return@launch
             }
-            if(response.isSuccessful && response.body() != null) {
+            if (response.isSuccessful && response.body() != null) {
                 objectIDs = response.body()!!.objectIDs
             } else {
                 Log.e(TAG, "Response not successful")
             }
         }
+        return objectIDs
     }
 
     fun getMuseumObject(objectID: Int): MuseumObject? {
@@ -81,10 +87,51 @@ class Repository () {
     }
 
     fun getNObjects(n: Int): List<MuseumObject> {
-        TODO()
+        val museumObjects = mutableListOf<MuseumObject>()
+        while (museumObjects.size < n) {
+            val objectID = objectIDs?.random() ?: (1..400_000).random()
+            CoroutineScope(Dispatchers.IO).launch {
+                val response = try {
+                    service.fetchMuseumObject(objectID)
+                } catch(e: IOException) {
+                    Log.e(TAG, "IOException, you might not have internet connection")
+                    return@launch
+                } catch (e: HttpException) {
+                    Log.e(TAG, "HttpException, unexpected response")
+                    return@launch
+                }
+                if(response.isSuccessful && response.body() != null) {
+                    museumObjects.add(response.body()!!)
+                } else {
+                    Log.e(TAG, "Response not successful")
+                }
+            }
+        }
+        return museumObjects.toList()
     }
 
     fun getNObjectsByDepto(n: Int, deptoId: Int): List<MuseumObject> {
-        TODO()
+        val departmentObjects = getObjectIDs(deptoId)
+        val museumObjects = mutableListOf<MuseumObject>()
+        while (museumObjects.size < n) {
+            val objectID = objectIDs?.random() ?: (1..400_000).random()
+            CoroutineScope(Dispatchers.IO).launch {
+                val response = try {
+                    service.fetchMuseumObject(objectID)
+                } catch(e: IOException) {
+                    Log.e(TAG, "IOException, you might not have internet connection")
+                    return@launch
+                } catch (e: HttpException) {
+                    Log.e(TAG, "HttpException, unexpected response")
+                    return@launch
+                }
+                if(response.isSuccessful && response.body() != null) {
+                    museumObjects.add(response.body()!!)
+                } else {
+                    Log.e(TAG, "Response not successful")
+                }
+            }
+        }
+        return museumObjects.toList()
     }
 }
